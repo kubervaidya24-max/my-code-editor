26cc07bfc07d4cd3c348f8e003155245ba898a75
Kuber Vaidya <2023uec1459@mnit.ac.in>
2026-07-17 14:17:20 +0530

    06:Temp selecting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["172.21.55.13", "localhost"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
